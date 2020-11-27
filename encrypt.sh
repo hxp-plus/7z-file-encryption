a90153c24529d0f21df33bd456bc88c11dc21113
hxp <hxp201406@gmail.com>
2020-11-27 17:37:03 +0800

    Initial commit

---
+++ encrypt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TARGET_DIR=$1
+RANDOM_STR=$(head /dev/random | tr -dc '[:alnum:]' | head -c 64)
+
+echo ${RANDOM_STR} > ./"${TARGET_DIR}.txt"
+7z a -p"${RANDOM_STR}" ${TARGET_DIR}.7z ${TARGET_DIR}
+7z a -p"${RANDOM_STR}" -sdel ${TARGET_DIR}.7z.7z ${TARGET_DIR}.7z
+mkdir -p encrypted
+mv ${TARGET_DIR} encrypted/${TARGET_DIR}
