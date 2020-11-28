edae096d14243603e9ea1a6e763fea5f5f429043
hxp <hxp201406@gmail.com>
2020-11-28 17:27:14 +0800

    modified:   encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -4,7 +4,6 @@ TARGET_DIR=$1
 RANDOM_STR=$(head /dev/random | tr -dc '[:alnum:]' | head -c 64)
 
 echo ${RANDOM_STR} > ./"${TARGET_DIR}.txt"
-7z a -p"${RANDOM_STR}" ${TARGET_DIR}.7z ${TARGET_DIR}
-7z a -p"${RANDOM_STR}" -sdel ${TARGET_DIR}.7z.7z ${TARGET_DIR}.7z
+7z a -p"${RANDOM_STR}" -mhe ${TARGET_DIR}.7z ${TARGET_DIR}
 mkdir -p encrypted
 mv ${TARGET_DIR} encrypted/${TARGET_DIR}
